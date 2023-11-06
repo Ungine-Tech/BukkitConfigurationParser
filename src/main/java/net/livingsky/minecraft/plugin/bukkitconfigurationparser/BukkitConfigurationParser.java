66dbe045e6470c42507ecba6c930b93fd60a12c8
mikoto <mikoto@ungine.cn>
2023-11-06 23:16:07 +0800

    init

---
+++ src/main/java/net/livingsky/minecraft/plugin/bukkitconfigurationparser/BukkitConfigurationParser.java
@@ -0,0 +1,17 @@
+package net.livingsky.minecraft.plugin.bukkitconfigurationparser;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BukkitConfigurationParser extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
